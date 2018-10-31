e3ee87abca718e8e66159f186072ca5dc88ca5f6
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-31 10:20:27 -0600

    Adds find artist with multiple photos

--- lib/curator.rb
+++ lib/curator.rb
@@ -23,4 +23,12 @@ class Curator
   def find_photograph_by_id(id)
     photographs.find {|photo| photo.id == id}
   end
+
+  def find_photographs_by_artist(artist)
+    photographs.find_all {|photo| photo.artist_id == artist.id}
+  end
+
+  def artists_with_multiple_photographs
+    artists.find_all {|artist| find_photographs_by_artist(artist).count > 1}
+  end
 end
